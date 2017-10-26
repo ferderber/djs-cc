4a785c85a01ee8e3a3baec8c2a2387c2e3a7111a
Matthew Ferderber <matthewferderber@gmail.com>
2017-10-26 18:10:31 -0400

    add default prefix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let CommandClient;
 try {
     CommandClient = require('./build');
     const client = new CommandClient.Client();
+    client.prefix = '?';
     client.login(process.env.DISCORD_BOT_TOKEN);
 } catch (e) {
     console.error('Build djs-cc with npm run build before running this sample! (or use npm run start)');
